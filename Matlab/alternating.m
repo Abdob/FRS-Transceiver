75f4c7e640d043f28776e817cdba07eaf74661f2
Abdo Babukr <abdo.babukr@gmail.com>
2019-07-27 12:38:22 -0700

    Trouble shooting new setup

--- Matlab/alternating.m
+++ Matlab/alternating.m
@@ -2,14 +2,15 @@
 
 beta = 0.5; % roll off factor for pulse shape
 span = 6;   % how many pulse periods each spans
-sps = 8;    % oversampling factor, samples per pulse
+sps = 64;    % oversampling factor, samples per pulse
 
 B = rcosdesign(beta, span, sps);
 plot(B); grid on; xlabel('sample'); title('Square-root raised cosine pulse');
 
 
 %% Generate a sequence of alternating SRRC pulses
-pattern = cos((1:60)*pi); % alternating pattern
+pattern = [1 1 -1 1 -1 -1 1 -1 1 1 -1 1 -1 -1 1 -1 1 1 -1 1 -1 -1 1 -1 1 1 -1 1 -1 -1 1 -1 ...% alternating pattern
+    1 1 -1 1 -1 -1 1 -1 1 1 -1 1 -1 -1 1 -1 1 1 -1 1 -1 -1 1 -1 1 1 -1 1 -1 -1 1 -1];
 pattern = upsample(pattern, sps); % upsample to meet pulse sampling rate
 sequence = filter(B, 1, pattern); % pulse shaping pattern into SRRC sequence
 figure(2); plot(sequence); grid on; xlabel('sample');title('SRRC alternating sequence');
